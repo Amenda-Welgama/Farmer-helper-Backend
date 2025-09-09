7bdf412c26f9b723b64bf85ba8366b4eb47cf9a8
Amenda-Welgama <amenda20020506@gmail.com>
2025-09-09 18:53:11 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "user"),
+        allowNull: false,
+        defaultValue: "user",
+      },
+    },
+    {
+      timestamps: true,
+      tableName: "Users",
+    }
+  );
+
+  return User;
+};
